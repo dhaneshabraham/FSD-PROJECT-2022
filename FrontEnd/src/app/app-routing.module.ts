2d0ca8d15a139e287297b8ad2fd8a581d1088576
unknown <pkshafna123@gmail.com>
2022-07-16 16:11:55 +0530

    Home page added

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { EnrollmentformComponent } from './enrollmentform/enrollmentform.component';
-
+import { EnrollmentformComponent } from './enrollmentform/enrollmentform.component';import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { LoginComponent } from './login/login.component';
 const routes: Routes = [
-  {path: "registration", component: EnrollmentformComponent}
+  {path: "registration", component: EnrollmentformComponent} ,
+  { path:'', component: HomeComponent },
+  { path:'home', component: HomeComponent },
+  { path:'about', component: AboutComponent },
+  { path:'footer', component: FooterComponent },
+  { path:'contactus', component: ContactusComponent },
+  { path:'login', component: LoginComponent }
 ];
 
 @NgModule({
--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -1,18 +1,44 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EnrollmentformComponent } from './enrollmentform/enrollmentform.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
+import { CoursesComponent } from './courses/courses.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { HomeHeaderComponent } from './home-header/home-header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EnrollmentformComponent
+    EnrollmentformComponent,
+    HomeComponent,
+    AboutComponent,
+    LoginComponent,
+    CoursesComponent,
+    ContactusComponent,
+    HomeHeaderComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    BrowserAnimationsModule,
+    MatBottomSheetModule
   ],
   providers: [],
   bootstrap: [AppComponent]
